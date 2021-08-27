0759eba909dff03d9a9b2196f4aa93c1276002de
yahel <yahelcohenko@gmail.com>
2021-08-27 18:21:06 +0300

    Fixed/improved compile.bat script

--- compile.bat
+++ compile.bat
@@ -3,18 +3,15 @@
 @REM Check if python is installed and alert user if not
 py -3 -V > nul 2>&1
 if errorlevel 1 (
-    echo Please install Python 3
+    call :echodq "Python 3 is required but not found (Failed to run 'py' command)"
     goto :end
 )
 
-
 @REM Make sure python dependecies are installed
-pyinstaller --help > nul 2>&1
-call :install_if_last_command_failed pyinstaller || goto :end
-call :require_package docxtpl || goto :end
-call :require_package csv || goto :end
-call :require_package winreg || goto :end
-
+pyinstaller -v > nul 2>&1 || call :install_package pyinstaller || goto :end
+call :ensure_package docxtpl || goto :end
+call :ensure_package csv || goto :end
+call :ensure_package winreg || goto :end
 
 @REM Compile project into build folder
 if not exist .\build mkdir .\build
@@ -24,21 +21,24 @@ goto :end
 
 
 @REM Functions
+goto :eof
+
+@REM Echos one parameter without quotation marks
+:echodq
+    echo %~1
+goto :eof
 
 @REM Installs a python package if the last command exited with error level 1
-:install_if_last_command_failed
-    if errorlevel 1 (
-        echo Package '%~1' not found, installing now
-        py -3 -m pip install %~1
-        exit /b 1
-    )
-    exit /b 0
+:install_package
+    call :echodq "Installing package '%~1'"
+    py -3 -m pip install %~1
+    if errorlevel 1 call :echodq "Failed installing package '%~1'"
+goto :eof
 
 @REM Check if a package is installed and install it if not
-:require_package
-    py -3 -c "import %~1" > nul 2>&1
-    call :install_if_last_command_failed %~1
-    exit /b %errorlevel%
+:ensure_package
+    py -3 -c "import %~1" > nul 2>&1 || call :install_package %~1
+goto :eof
 
 :end
     if "%~1" == "" @pause
