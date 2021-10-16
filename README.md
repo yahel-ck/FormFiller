d0c8acef4978ace21458006325debed36bd72c24
Yahel Cohen <yahelcohenko@gmail.com>
2021-10-16 14:44:29 +0300

    Removed compile.bat, added compile.py

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FormFiller
-A script to fill forms automatically with client details
+A script to fill forms automatically with with predefined details.
+
+### Compile into a executable
+This repo includes the script `compile.py` which uses the `pyinstaller` package
+to compile the python script into an executable.
+
+To create an executable for your machine, simply run:
+```
+python compile.py
+```
---
+++ compile.py
@@ -0,0 +1,80 @@
+import shlex
+import sys
+from os import chdir, mkdir
+from os.path import basename, isdir, isfile
+from subprocess import DEVNULL
+from subprocess import run as run_proc
+
+# The path to store the compilation output
+BUILD_DIR_PATH = './build'
+# The path to the script to compile, relative to build folder
+SCRIPT_PATH = '../formfiller.py'
+# Commands
+PYTHON_CMD = 'python'
+PIP_CMD = PYTHON_CMD + ' -m pip --no-input'
+CHECK_PACKAGE_CMD = PIP_CMD + ' show {0}'
+INSTALL_PACKAGE_CMD = PIP_CMD + ' install {0}'
+PYINSTALLER_CMD = 'pyinstaller --exclude-module _bootlocale -F {0}'
+
+
+def soft_assert(condition_result, error_message):
+    """
+    Checks the given condition and raises an exception if it's false.
+    Unlike normal assert, this method raises an exception without printing the 
+    traceback, only the given error_message.
+    """
+    if not condition_result:
+        raise SystemExit('ERROR: {}'.format(error_message))
+
+
+def run(cmd, quiet=True, **kwargs):
+    cmd_args = shlex.split(cmd)
+    if not quiet or is_debug:
+        print('Running command: {}'.format(cmd))
+        return run_proc(cmd_args, **kwargs)
+    else:
+        return run_proc(cmd_args, stdout=DEVNULL, stderr=DEVNULL, **kwargs)
+
+
+def install_package(package_name):
+    print("Installing package {}".format(package_name))
+    cmd = run(INSTALL_PACKAGE_CMD.format(package_name))
+    soft_assert(
+        cmd.returncode == 0,
+        "Failed to install package '{}', exiting script".format(package_name))
+
+
+def ensure_package(package_name):
+    cmd = run(CHECK_PACKAGE_CMD.format(package_name))
+    if cmd.returncode != 0:
+        install_package(package_name)
+
+
+def compile_script(script_path):
+    print('Compilation starting...')
+    cmd = run(PYINSTALLER_CMD.format(script_path))
+    soft_assert(cmd.returncode == 0, "Compilation failed :(")
+    print('Compiled successfully :)')
+
+
+def main():
+    # Make sure python dependencies are installed
+    ensure_package('docxtpl')
+    ensure_package('pyinstaller')
+
+    # Create and enter build folder
+    if not isdir('./build'):
+        mkdir('./build')
+    chdir('./build')
+
+    # Make sure the script exits
+    soft_assert(isfile(SCRIPT_PATH), "Can't find '{}' script to compile"
+                .format(basename(SCRIPT_PATH)))
+
+    compile_script(SCRIPT_PATH)
+    print('Executable should be found under build/dist folder')
+
+
+if __name__ == '__main__':
+    is_debug = '--debug' in sys.argv
+    main()
