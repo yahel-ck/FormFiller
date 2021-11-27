f151ecf6a457d3954e2cb0ba1097c330d59a22b4
Yahel Cohen <yahelcohenko@gmail.com>
2021-11-27 19:18:06 +0200

    Added GUI using tkinter

--- formfiller.py
+++ formfiller.py
@@ -1,10 +1,13 @@
-import sys
-from docxtpl import DocxTemplate
 import csv
-from os.path import basename, splitext, join as joinpath
-import winreg
-from subprocess import check_call
+import tkinter
 from datetime import date
+from os.path import basename
+from os.path import join as joinpath
+from os.path import splitext
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+
+from docxtpl import DocxTemplate
 
 
 CSV_PROG_ID_PATH = '.csv'
@@ -16,37 +19,50 @@ CURRENT_DATE_KEY = 'current_date'
 
 
 def main():
-    if len(sys.argv) < 2:
-        setup_formfiller()
-    else:
-        fill_forms()
+    root = tkinter.Tk()
+    root.title('Form Filler')
+    # root.geometry('500x200')
+
+    # Client details selection
+    details_path_entry = build_file_selection_row(
+        root, 0, 'Client details file:')
+
+    # Template selection
+    template_path_entry = build_file_selection_row(root, 1, 'Template file:')
+
+    def submit():
+        params_path = details_path_entry.get()
+        template_path = template_path_entry.get()
+        fill_forms(template_path, params_path)
+
+    submit_button = tkinter.Button(root, text='Fill form', command=submit)
+    submit_button.grid(column=0, row=3)
+
+    root.mainloop()
 
 
-def setup_formfiller():
-    try:
-        set_reg(winreg.HKEY_CLASSES_ROOT, CSV_PROG_ID_PATH, CSV_PROG_ID)
-        set_reg(winreg.HKEY_CURRENT_USER,
-                RIGHT_CLICK_OPTION_PATH,
-                RIGHT_CLICK_TITLE)
-        set_reg(winreg.HKEY_CURRENT_USER,
-                joinpath(RIGHT_CLICK_OPTION_PATH, 'command'),
-                '"{}" "%1"'.format(sys.argv[0]))
-        print('Installed successfully!')
-    except PermissionError:
-        print('Please run as administrator to install FormFiller')
-    pause()
+def build_file_selection_row(root, row, label_text):
+    ttk.Label(root, text=label_text).grid(row=row, column=0)
+    entry = ttk.Entry(root)
+    entry.grid(row=row, column=1)
+
+    def browse_file():
+        filename = askopenfilename()
+        entry.delete(0, 'end')
+        entry.insert(0, filename)
+
+    button = tkinter.Button(root, text='Browse', command=browse_file)
+    button.grid(row=row, column=2)
+
+    return entry
 
 
-def fill_forms():
-    params_path = get_params_path()
-    print('Parameters: {}'.format(params_path))
+def fill_forms(template_path, params_path):
+    print('Template path: {}'.format(template_path))
+    print('Params path: {}'.format(params_path))
 
-    template_path = get_template_path()
-    print('Template: {}'.format(template_path))
-
-    with open(params_path, encoding='utf8') as fp:
-        params = [row[:2] for row in csv.reader(fp)]
-    params.append([CURRENT_DATE_KEY, date.today().strftime('%d/%m/%Y')])
+    params = get_params(params_path)
+    print('Params: {}'.format(params))
 
     doc = DocxTemplate(template_path)
     doc.render(params)
@@ -54,36 +70,19 @@ def fill_forms():
         generate_file_name(params_path, template_path)))
 
     print('Filled form successfully!')
-    pause()
 
 
-def get_params_path():
-    # TODO Make sure it's a CSV file and alert user if not
-    if len(sys.argv) > 1:
-        return sys.argv[1]
-    return input('Enter parameters file path: ')
-
-
-def get_template_path():
-    # TODO Make sure it's a DOCX file and alert user if not
-    return input('Enter template file path: ')
+def get_params(params_path):
+    with open(params_path, encoding='utf8') as fp:
+        params = [row[:2] for row in csv.reader(fp)]
+    params.append([CURRENT_DATE_KEY, date.today().strftime('%d/%m/%Y')])
+    return params
 
 
 def generate_file_name(params_path, template_path):
     params_name = splitext(basename(params_path))[0]
     template_name = splitext(basename(template_path))[0]
     return '{}_{}'.format(template_name, params_name)
-
-
-def set_reg(key, sub_path, value, name=None):
-    winreg.CreateKey(key, sub_path)
-    regkey = winreg.OpenKey(key, sub_path, 0, winreg.KEY_WRITE)
-    winreg.SetValueEx(regkey, name, 0, winreg.REG_SZ, value)
-    winreg.CloseKey(regkey)
-
-
-def pause():
-    check_call(['pause'], shell=True)
 
 
 if __name__ == '__main__':
