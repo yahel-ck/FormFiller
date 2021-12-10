83daf42f32ef2b93712e800ac4e81f73fb21fba0
Yahel Cohen <yahelcohenko@gmail.com>
2021-12-10 17:45:38 +0200

    Save last selected files + tweaked entries UI

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tkinter.ttk as tk
-from os.path import basename, dirname
+from os import makedirs
+from os.path import basename, dirname, expanduser, exists
 from os.path import join as joinpath
 from os.path import splitext
 from tkinter import Tk, messagebox
@@ -11,6 +12,9 @@ from formfiller import fill_form, read_params
 
 PARAMS_FILE_TYPES = (('CSV file', '*.csv'), ('JSON file', '*.json'))
 TEMPLATE_FILE_TYPES = (('Word file', '*.docx'), ('PDF file', '*.pdf'))
+PARAM_FILE_SETTING_NAME = 'param_file_entry'
+TEMPLATE_FILE_SETTING_NAME = 'template_file_entry'
+APP_DIR = '.formfiller'
 
 
 def main():
@@ -22,11 +26,12 @@ def main():
 
     # Client details selection
     details_path_entry = build_file_selection_row(
-        root, 0, 'Client', PARAMS_FILE_TYPES, pady=(8, 2))
+        root, 0, 'Client', PARAMS_FILE_TYPES, PARAM_FILE_SETTING_NAME, 
+        pady=(8, 2))
 
     # Template selection
     template_path_entry = build_file_selection_row(
-        root, 1, 'Form', TEMPLATE_FILE_TYPES)
+        root, 1, 'Form', TEMPLATE_FILE_TYPES, TEMPLATE_FILE_SETTING_NAME)
 
     def submit():
         params_path = details_path_entry.get()
@@ -58,17 +63,20 @@ def main():
     root.mainloop()
 
 
-def build_file_selection_row(root, row, label_text, filetypes, pady=0):
+def build_file_selection_row(root, row, label_text, filetypes, setting_name=None, pady=0):
     label = tk.Label(root, text=label_text)
     label.grid(row=row, column=0, padx=(10, 10), pady=pady, sticky='w')
 
-    entry = tk.Entry(root)
+    entry = tk.Entry(root, width=45)
     entry.grid(row=row, column=1, padx=8, pady=pady, sticky='we')
+    entry.insert(0, load_setting(setting_name))
 
     def browse_file():
         filename = askopenfilename(filetypes=filetypes)
+        save_setting(setting_name, filename)
         entry.delete(0, 'end')
         entry.insert(0, filename)
+        entry.xview("end")
 
     button = tk.Button(root, text='Browse', command=browse_file)
     button.grid(row=row, column=2, padx=(0, 8), pady=pady, sticky='e')
@@ -82,6 +90,24 @@ def generate_output_path(params, template_path, file_extension):
     template_name = splitext(basename(template_path))[0]
     file_name = '{}_{}.{}'.format(params_name, template_name, file_extension)
     return joinpath(folder, file_name)
+
+
+def load_setting(setting_name):
+    if setting_name is None:
+        return ''
+    try:
+        with open(joinpath(expanduser('~'), APP_DIR, setting_name), 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        return ''
+
+
+def save_setting(setting_name, value):
+    dir_path = joinpath(expanduser('~'), APP_DIR)
+    print(dir_path)
+    makedirs(dir_path, exist_ok=True)
+    with open(joinpath(dir_path, setting_name), 'w') as f:
+        f.write(value)
 
 
 if __name__ == '__main__':
