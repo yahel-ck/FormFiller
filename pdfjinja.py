547e188b314c198d366210a9c14335624bd6bfc0
Yahel Cohen <yahelcohenko@gmail.com>
2021-12-15 22:42:21 +0200

    Refactored pdfjinja.py

--- pdfjinja.py
+++ pdfjinja.py
@@ -5,11 +5,18 @@ from PyPDF2.utils import b_, str_, ord_
 import re
 
 
+TEXT_OPERATOR = b_("Tj")
+NEWLINE_TEXT_OPERATOR = b_("'")
+SPACING_NEWLINE_TEXT_OPERATOR = b_('"')
+POSITIONED_GLYPHS_OPERATOR = b_("TJ")
+FONT_OPERATOR = b_("Tf")
+
 CMAP_ENTRY_RE = re.compile("\\s*<([0-9a-fA-F]+)>\\s+<([0-9a-fA-F]+)>\\s*")
 CMAP_RE = re.compile(
     "\nbegincmap\n(?:.*?\n)?[0-9]* beginbfchar\n(.*?)\nendbfchar\n(?:.*?\n)?"
     "endcmap\n", 
-    re.DOTALL)
+    re.DOTALL
+)
 
 
 def map_text_objects(page, content_stream, mapper_func):
@@ -17,57 +24,59 @@ def map_text_objects(page, content_stream, mapper_func):
     Iterates over each string object in the given ContentStream, and applies the
     given function to it.
 
-    Note: Works only for TextStringObjects or ByteStringObjects with cmap
+    Note: Works only for TextStringObjects, or ByteStringObjects with cmap
     defined. ByteStringObjects are strings where the byte->cstring encoding was 
     unknown, but if they have a cmap field we can translate them to unicode,
     otherwise their text here would be gibberish.
     """
+    cmap = None
+    cmaps = {}
+
     def map_text_object(parent, index):
-        obj = translate(parent[index])
-        if obj is None:
+        obj = parent[index]
+        if isinstance(obj, ByteStringObject):
+            obj = decode(obj, cmap)
+        elif not isinstance(obj, TextStringObject):
             return
+
         replacement = mapper_func(obj)
         if isinstance(replacement, TextStringObject):
             parent[index] = replacement
-        else:
+        elif replacement is not None:
             parent[index] = createStringObject(replacement)
 
-    c_map = None
-    c_maps = {}
-    # Concatenate TextStringObjects and try to translate ByteStringObjects
-    # when we have a CMap, when we don't, then byte->string encoding is unknown,
-    # so adding them to the text here would be gibberish.
-
-    def translate(text):
-        if isinstance(text, TextStringObject):
-            return text
-        if isinstance(text, ByteStringObject):
-            newText = "".join(c_map.get(ord_(c), '?') for c in text)
-            return newText
-        return None
-
     for operands, operator in content_stream.operations:
-        if operator == b_("Tf"):
+        if operator == FONT_OPERATOR:
             try:
                 font = operands[0]
-                c_map = c_maps.get(font)
-                if (c_map == None):
-                    c_map = parse_cmap(get_cmap_data(page, font))
-                    c_maps[font] = c_map
+                cmap = cmaps.get(font)
+                if (cmap == None):
+                    cmap = parse_cmap(get_cmap_data(page, font))
+                    cmaps[font] = cmap
             except KeyError:
-                c_map = None
-        elif operator in (b_("Tj"), b_("'")):
+                cmap = None
+        elif operator in (TEXT_OPERATOR, NEWLINE_TEXT_OPERATOR):
             map_text_object(operands, 0)
-        elif operator == b_('"'):
+        elif operator == SPACING_NEWLINE_TEXT_OPERATOR:
             map_text_object(operands, 2)
-        elif operator == b_("TJ"):
+        elif operator == POSITIONED_GLYPHS_OPERATOR:
             objs = operands[0]
             for i in range(len(objs)):
                 map_text_object(objs, i)
 
 
+def decode(text, cmap):
+    """
+    Decodes a ByteStringObject using the given cmap.
+    """
+    newText = "".join(cmap.get(ord_(c), u'\uFFFD') for c in text)
+    return newText
+
+
 def get_cmap_data(page, font):
-    return str_(page["/Resources"]["/Font"][font]["/ToUnicode"].getData())
+    to_unicode_obj = page["/Resources"]["/Font"][font]["/ToUnicode"]
+    print(to_unicode_obj)
+    return str_(to_unicode_obj.getData())
 
 
 def parse_cmap(cstr):
@@ -95,9 +104,6 @@ class PDFTemplate(object):
         Translates the given text using the template's parameters.
         """
         new_text = self.jinja_env.from_string(text).render(self.params)
-        print(' '.join(str(ord(c)) for c in text))
-        print(' '.join(str(ord(c)) for c in new_text))
-        print()
         return new_text
 
     def save(self, output_path):
