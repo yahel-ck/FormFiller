d186a7a985112971416dc3f84e1d5bdca9780940
Yahel Cohen <yahelcohenko@gmail.com>
2021-12-16 23:11:35 +0200

    Fixed imports , fixed command for windows

--- compile.py
+++ compile.py
@@ -2,21 +2,20 @@
 
 import shlex
 import sys
-from os.path import basename, splitext, expanduser, isfile, isdir
+from os.path import basename, splitext
 from os.path import join as joinpath
-from subprocess import DEVNULL
 from subprocess import run as run_proc
-from pycrosskit.shortcuts import Shortcut
-from PyInstaller.__main__ import run as run_pyinstaller
 
 SCRIPT_PATH = 'app.py'
 ICON_PATH = 'icon.ico'
 WINDOW_ICON_PATH = 'icon.png'
 APP_NAME = 'Form Filler'
-ENSURE_PACKAGES = ('docxtpl', 'pyinstaller', 'pandas', 'openpyxl', 'PyPdf2')
+ENSURE_PACKAGES = (
+    'docxtpl', 'pyinstaller', 'pandas', 'openpyxl', 'PyPdf2', 'pycrosskit'
+)
 
 # Commands
-PIP_CMD = sys.executable + ' -m pip --no-input'
+PIP_CMD = '"{}" -m pip --no-input'.format(sys.executable)
 CHECK_PACKAGE_CMD = PIP_CMD + ' show {0}'
 INSTALL_PACKAGE_CMD = PIP_CMD + ' install {0}'
 PYINSTALLER_CMD = (
@@ -42,7 +41,7 @@ def main():
 
 def run(cmd, **kwargs):
     cmd_args = shlex.split(cmd)
-    print('Running command: {}'.format(cmd))
+    print('Running: {}'.format(cmd_args))
     return run_proc(cmd_args, **kwargs)
 
 
@@ -59,7 +58,7 @@ def install_package(package_name):
 
 
 def compile_script(script_path, app_name, icon_path, window_icon):
-    print('Compilation starting...')
+    from PyInstaller.__main__ import run as run_pyinstaller
     cmd = PYINSTALLER_CMD.format(
         python_file=script_path,
         icon_path=icon_path,
@@ -71,6 +70,7 @@ def compile_script(script_path, app_name, icon_path, window_icon):
 
 
 def create_desktop_shortcut(file_path, shortcut_name=None):
+    from pycrosskit.shortcuts import Shortcut
     return Shortcut(
         shortcut_name or splitext(basename(file_path))[0], 
         file_path,
